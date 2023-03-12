b84e02891566f72314637d51004c8698e3755380
Sachin S <101466465+Sachin-S-dev@users.noreply.github.com>
2023-03-12 16:20:34 +0530

    Update StudentRepository.java

--- src/main/java/com/sachin/spring/data/mongodb/repository/StudentRepository.java
+++ src/main/java/com/sachin/spring/data/mongodb/repository/StudentRepository.java
@@ -8,9 +8,5 @@ import com.sachin.spring.data.mongodb.model.Student;
 
 public interface StudentRepository extends MongoRepository<Student, String> {
 
-	  Student findByAdmissionNumber(String admissionNumber);
-	    
-	  List<Student> findByClassvalAndDivision(String classval, String division);
-
-	  List<Student> findAllByOrderByNameAsc();
+	 List<Student> findAll();
 }
