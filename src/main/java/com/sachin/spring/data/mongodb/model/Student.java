9c681ab654536a968ad91ad3b8242ea707147e6d
Sachin S <101466465+Sachin-S-dev@users.noreply.github.com>
2023-03-12 16:19:31 +0530

    Update Student.java

--- src/main/java/com/sachin/spring/data/mongodb/model/Student.java
+++ src/main/java/com/sachin/spring/data/mongodb/model/Student.java
@@ -36,7 +36,15 @@ public Student(String name, String dob,String classval, String division, String 
  
 }
 
-  public String getName() {
+	
+	public String getId() {
+	       return id;
+        }
+
+        public void setId(String id) {
+	        this.id = id;
+        }
+        public String getName() {
 		return name;
 	}
 
